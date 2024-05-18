9acb1833e5d98a924a051c891224f786370b24dc
Arroch35 <arrocha.0305@gmail.com>
2024-05-18 14:51:28 +0200

    "App initialiced"

---
+++ app/src/androidTest/java/com/example/ferrovisibility/ExampleInstrumentedTest.java
@@ -0,0 +1,21 @@
+package com.example.ferrovisibility;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+import android.content.Context;
+
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.ferrovisibility", appContext.getPackageName());
+    }
+}
